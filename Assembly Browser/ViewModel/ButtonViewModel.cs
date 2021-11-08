29c0e7cbe4be0e7554c780b4e432e48e4e6073cf
Redflame39 <vanya133719@gmail.com>
2021-11-08 23:18:35 +0300

    containers

--- Assembly Browser/ViewModel/ButtonViewModel.cs
+++ Assembly Browser/ViewModel/ButtonViewModel.cs
@@ -2,12 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using Assembly_Browser;
 
 namespace Assembly_Browser.ViewModel
 {
